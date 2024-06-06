49fa4a6886e4caa20f71c49845763a911d0e9c38
fengxd <9129057+fxd2000@user.noreply.gitee.com>
2024-06-06 15:36:44 +0800

    2024-06-06

---
+++ app/ui/acme-logo.tsx
@@ -0,0 +1,14 @@
+import { AcademicCapIcon, GlobeAltIcon } from '@heroicons/react/24/outline';
+import { lusitana } from '@/app/ui/fonts';
+
+export default function AcmeLogo() {
+  return (
+    <div
+      className={`${lusitana.className} flex flex-row items-center leading-none text-white`}
+    >
+      {/* <GlobeAltIcon className="h-12 w-12 rotate-[15deg]" /> */}
+      <AcademicCapIcon className="h-12 w-12 rotate-[15deg]"/>
+      <p className="text-[20px]">报销签收</p>
+    </div>
+  );
+}
